4a56c8d1639bfff115a81be689f01e24feca4279
CristhyAcevedo <20242486@itla.edu.do>
2026-02-03 00:29:23 -0400

    Add files via upload

---
+++ consultas_resultados_911.sql
@@ -0,0 +1,15 @@
+USE Emergencias_911;
+GO
+
+SELECT COUNT(*) AS Total
+FROM Llamadas_Emergencia;
+
+
+
+SELECT * FROM Llamadas_Emergencia;
+
+
+SELECT COUNT(*) AS Total
+FROM Llamadas_Emergencia;
+
+SELECT * FROM Llamadas_Emergencia
